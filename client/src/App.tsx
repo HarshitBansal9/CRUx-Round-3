5851011c7e44c15cfe9d1a9ea6adda0fc2bbed50
HarshitBansal9 <90168787+HarshitBansal9@users.noreply.github.com>
2024-05-30 23:20:08 +0530

    feat:user can now logout after logging in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,33 @@ import Navbar from './components/Navbar';
 import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import { useEffect,useState } from 'react';
 const App = () =>{
-  const user:boolean = false;
+  const [user,setUser] = useState(null);
+  useEffect(()=>{
+    const getUser =  async ()=>{
+      await fetch("http://localhost:5000/auth/login/success",{
+        method:"GET",
+        credentials:"include",
+        headers:{
+          "Content-Type":"application/json",
+          Accept:"application/json",
+          "Access-Control-Allow-Credentials":"true",
+        },
+    }).then((res)=>{
+      if (res.status === 200){
+        return res.json();
+      }
+      throw new Error("Failed to authenticate user");
+    }).then((resJson:any)=>{
+      setUser(resJson.user)
+    }).catch((err)=>{
+      console.log(err);
+    })
+    };
+    getUser();
+  },[])
+  console.log(user);
   return (
     <BrowserRouter>
     <div>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom'
 
 const Navbar = ({user}:any) => {
+  const Logout = ()=>{
+    window.open("http://localhost:5000/auth/logout","_self")
+  };
   return (
     <div className="navbar w-full h-[50px] bg-slate-600 flex items-center flex-row justify-evenly">
       {user ? (
       <div className='w-full flex justify-evenly items-center'>
         <div className='font-bold text-xl text-white'>Logo</div>
-        <div className='font-bold text-xl text-white hover:cursor-pointer'>Logout</div>
+        <div  className='font-bold text-xl text-white hover:cursor-pointer' onClick={Logout}>Logout</div>
       </div>
       )
       :(<div className='w-full flex justify-evenly items-center'>
